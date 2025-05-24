a167dbf919b349506626d50ac8c7597f3698eaee
Vidushi Singhal <singhalvidushi7417529406@gmail.com>
2025-05-24 10:31:16 +0530

    Add code

---
+++ Ambu-Web-App-main/app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from app.controller import controllers
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['DEBUG'] = True
+
+    # Register the blueprints for endpoints in app instance
+    app.register_blueprint(controllers.get_index_bp)
+    app.register_blueprint(controllers.get_Sign_Up)
+    app.register_blueprint(controllers.get_about_page_bp)
+
+    return app
